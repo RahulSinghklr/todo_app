13ccc7c311e93a05f4945ae9192f261bb20e73a8
RahulSinghklr <rahulshiksingh@gmail.com>
2025-02-22 23:01:38 +0530

    updated Server 3

--- Backend/server.js
+++ Backend/server.js
@@ -10,12 +10,17 @@ app.use(cors()); // If using CORS
 
 app.use('/api', todoRoutes); // Make sure `todoRoutes` is correctly imported
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
-.then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("MongoDB Connection Error:", err));
+
+  module.exports = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+    } catch (error) {
+        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+    }
+  };
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
